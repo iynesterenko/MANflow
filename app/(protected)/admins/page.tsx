4fc3027e55df831536fc2accc9b00e36fbd0fdd4
Inest <ivan.y.nesterenko@gmail.com>
2026-08-12 20:12:21 +0200

    renamed function

--- app/(protected)/admins/page.tsx
+++ app/(protected)/admins/page.tsx
@@ -1,4 +1,4 @@
-export default function ForgotPassword() {
+export default function Адміни() {
   return (
     <div className="text-center">
       <h2 className="text-lg font-semibold text-slate-800">Адміни</h2>
